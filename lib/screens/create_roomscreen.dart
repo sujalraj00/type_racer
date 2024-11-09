096ec2e03c47b4f8ca6122a1a69b3d9a12c3a17e
Sujal Raj <sujalraj611@gmail.com>
2024-11-09 22:32:46 +0530

    create and join room ui created

---
+++ lib/screens/create_roomscreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateRoomScreen extends StatelessWidget {
+  const CreateRoomScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Create Room'),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final bool isHome;
+  final VoidCallback onTap; // using function instead of voidcallback won't work for us bcox with that u will be calling the fn and not storing the address of fun
+  const CustomButton({super.key, required this.text, required this.isHome, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return ElevatedButton(
+      onPressed: onTap, 
+      child: Text(text,
+       style: const TextStyle(
+        fontSize: 16, color: Colors.white
+       ),),
+       style: ElevatedButton.styleFrom(
+        minimumSize: Size ( !isHome ?  width : width/4, 50),
+       ),
+      );
+  }
+}

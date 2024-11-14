4b5f4859171fcb5599e173915c9a3a0e446f085f
Sujal Raj <sujalraj611@gmail.com>
2024-11-14 23:20:11 +0530

    join room screen added

--- lib/screens/join_room_screen.dart
+++ lib/screens/join_room_screen.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:type_racer/widgets/custom_button.dart';
+import 'package:type_racer/widgets/custom_textfield.dart';
 
-class JoinRoomScreen extends StatelessWidget {
+class JoinRoomScreen extends StatefulWidget {
   const JoinRoomScreen({super.key});
 
   @override
+  State<JoinRoomScreen> createState() => _JoinRoomScreenState();
+}
+
+class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  final _nameController = TextEditingController();
+  final _gameIdController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _nameController.dispose();
+    _gameIdController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Center(
-        child: Text('Join Room'),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: 600
+          ),
+          child: Container(
+            margin: const EdgeInsets.symmetric( horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Join Room', style: TextStyle(fontSize: 30),),
+                SizedBox(height: size.height*0.08,),
+                CustomTextField(controller: _nameController, 
+                hintText: 'Enter your nickname'),
+                  const SizedBox(height: 30,),
+                 CustomTextField(controller: _gameIdController, 
+                hintText: 'Enter Game Id'),
+                const SizedBox(height: 30,), 
+                CustomButton(text: 'Join',  onTap: (){})
+              ],
+            ),
+          ),
+          ),
       ),
     );
   }

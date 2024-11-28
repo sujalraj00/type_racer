dd24970cd3cf3db44fc0918b0f14eab9afde4c03
Sujal Raj <sujalraj611@gmail.com>
2024-11-28 19:54:15 +0530

    game state and game state provider updated

--- lib/models/game_state.dart
+++ lib/models/game_state.dart
@@ -4,14 +4,16 @@ class GameState{
   final bool isJoin;
   final bool isOver;
   final List words;
+  final String shortCode;
 
-  GameState({required this.id, required this.players, required this.isJoin, required this.isOver, required this.words});
+  GameState({required this.id, required this.players, required this.isJoin, required this.isOver, required this.words, required this.shortCode});
 
   Map<String, dynamic> toJson() =>{
     'id' : id,
     'players' : players,
     'isJoin' : isJoin,
     'words' : words,
-    'isOver' : isOver 
+    'isOver' : isOver ,
+    'shortCode' : shortCode
   };  
 }
--- lib/providers/game_state_provider.dart
+++ lib/providers/game_state_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:type_racer/models/game_state.dart';
 
 class GameStateProvider  extends ChangeNotifier{
-  GameState _gameState = GameState(id: '', players: [], isJoin: true, isOver: false, words: []);
+  GameState _gameState = GameState(id: '', players: [], isJoin: true, isOver: false, words: [], shortCode: '' );
 
   Map<String, dynamic> get gameState => _gameState.toJson();
 
@@ -11,14 +11,16 @@ class GameStateProvider  extends ChangeNotifier{
     required players, 
     required isJoin, 
     required isOver, 
-    required words
+    required words,
+    required shortCode
     }) {
       _gameState = GameState(
         id: id, 
         players: players, 
         isJoin: isJoin, 
         isOver: isOver, 
-        words: words
+        words: words, 
+        shortCode: shortCode.toString()
         );
 
         notifyListeners();

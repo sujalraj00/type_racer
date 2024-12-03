79f62c59f5e38d9ad613a60d51f32a1c320b8a2c
Sujal Raj <sujalraj611@gmail.com>
2024-12-04 02:01:56 +0530

    game screen updated

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -99,7 +99,7 @@ class _GameScreenState extends State<GameScreen> {
                                       width: 130,
                                       child: QrImageView(
                                         data:
-                                            'https://type-racer-ivory.vercel.app/join/${game.gameState['shortCode']}', 
+                                            "https://type-racer-ivory.vercel.app/join/${game.gameState['shortCode']}", 
                                         version: QrVersions.auto,
                                         size: 150.0,
                                       ),
